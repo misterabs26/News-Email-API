fc2f7e9295997a996584bafbd7d57dfc1831aea4
misterabs26 <mister.abs26@gmail.com>
2025-04-10 19:39:13 +0800

    Access the news titles and description

---
+++ main.py
@@ -0,0 +1,17 @@
+import requests
+
+api_key = "60d55ee3ea5442889806dcc059beb25b"
+url = ("https://newsapi.org/v2/everything?q=tesla&"
+       "from=2025-03-10&sortBy=publishedAt&apiKey="
+       "60d55ee3ea5442889806dcc059beb25b")
+
+# Make request
+request = requests.get(url)
+
+# Get a dictionary with data
+content = request.json()
+
+# Access the article titles and description
+for article in content["articles"]:
+    print(article["title"])
+    print(article["description"])
