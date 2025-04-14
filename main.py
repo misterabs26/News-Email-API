cddbadc3dae2a1aea909deff10e34fc3807382dd
misterabs26 <mister.abs26@gmail.com>
2025-04-14 19:24:20 +0800

    Send an email with news titles and description

--- main.py
+++ main.py
@@ -1,11 +1,16 @@
 import requests
 import streamlit as st
 import send_email as fn
+from datetime import date
+from dateutil.relativedelta import relativedelta
+
+# Get last month's date
+last_month = date.today() - relativedelta(months=1)
+formatted_date = last_month.strftime("%Y-%m-%d")
 
 api_key = "60d55ee3ea5442889806dcc059beb25b"
-url = ("https://newsapi.org/v2/everything?q=tesla&"
-       "from=2025-03-10&sortBy=publishedAt&apiKey="
-       "60d55ee3ea5442889806dcc059beb25b")
+url = (f"https://newsapi.org/v2/everything?q=tesla&from={formatted_date}"
+       f"&sortBy=publishedAt&apiKey=60d55ee3ea5442889806dcc059beb25b")
 
 # Make request
 request = requests.get(url)
@@ -14,6 +19,10 @@ request = requests.get(url)
 content = request.json()
 
 # Access the article titles and description
+body = []
 for article in content["articles"]:
-    print(article["title"])
-    print(article["description"])
+    body.append({
+        "title": article["title"],
+        "description": article["description"]
+    })
+fn.send_email(body)
--- send_email.py
+++ send_email.py
@@ -10,19 +10,22 @@ def send_email(message):
     admin_email = st.secrets["email"]["ADMIN_EMAIL"]
     admin_pw = st.secrets["email"]["ADMIN_PASS"]
 
+    # News Data
+    body = "Here are the latest Tesla news articles:\n\n"
+    for news in message:
+        news_title = news["title"]
+        news_desc = news["description"]
+        body += news_title +"\n" + news_desc + 2*"\n"
+
     # Email format
-    subject = "Daily News"
-    body = f"""
-    {message}
-    """
+    subject = "Tesla News Digest"
     msg = MIMEText(body)
     msg["From"] = admin_email
     msg["To"] = admin_email
     msg["Subject"] = subject
 
-
     context = ssl.create_default_context()
-    with smtplib.SMTP_SSL(host,port,context =context) as server:
+    with smtplib.SMTP_SSL(host,port,context=context) as server:
         server.login(admin_email,admin_pw)
         server.sendmail(admin_email,admin_email,msg.as_string())
 
